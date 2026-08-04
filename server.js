c7e748a59c80d7a5faa5e5910807b3b2a223615c
dhearenata534-art <dhearenata534@gmail.com>
2026-08-04 12:26:27 +0700

    tambah GET kategori dan GET produk

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const mysql2 = require('mysql2');
+
+const db = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database GlowList');
+    }
+});
 const PORT = 3001;
 
 app.use(express.json());
@@ -7,6 +23,30 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Selamat Datangg di GlowList APIIIII-!!!!');
 });
+
+// ============== GET produk =============== //
+
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+// ======================================== //
+
+// ============== GET kategori =============== //
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+// ======================================== //
 
 app.listen(PORT, () => {
     console.log(`Server GlowList jalan di http://localhost:${PORT}`);
